445d36384ac555622c35f42b8e4a5748e9d1392b
nerajno <nerajno@gmail.com>
2021-01-09 21:45:03 -0500

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -14,7 +14,15 @@ def first_wa(arr)
 end
 
 def first_wa(arr)
-	arr.each {|ele|
-		return ele if ele[0..1]=="wa"
-	}
+	arr.each {|item| return item if item[0..1]=="wa" }
+end
+
+def remove_non_strings(arr)
+    new_arr = []
+    arr.find {|item| new_arr.push(item) if item.class == String }
+    return new_arr
+end
+
+def count_elements(arr)
+     return arr.length
 end
